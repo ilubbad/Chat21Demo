9fda094f4e6dd3c6b276cd115f9bd0f662a516fa
ibraheemlubbad <ibraheem.a.lubbad@gmail.com>
2019-09-30 17:59:06 +0300

    setup chat21

--- app/src/main/java/chat21/android/demo/HomeFragment.java
+++ app/src/main/java/chat21/android/demo/HomeFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+
 import org.chat21.android.ui.contacts.activites.ContactListActivity;
 
 public class HomeFragment extends Fragment {
